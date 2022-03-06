e79b20aaf4c33ed0721401706e77cd45a85b8a61
Operator873 <operator873@gmail.com>
2022-03-05 18:42:52 -0600

    Some comment docs

--- CabalBot.py
+++ CabalBot.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0, "/path/to/.sopel/plugins/cabalbot")
+sys.path.insert(0, "/path/to/.sopel/plugins/cabalbot") # Help Sopel find the submodules we are about to load
 
 import affeed
 import rcfeed
@@ -15,82 +15,105 @@ import threading
 from sopel import plugin
 from sseclient import SSEClient as EventSource
 
-
+# Set some globals because repeating yourself is annoying
 BOTADMINMSG = "This command is only available to the bot admins."
 CHANCMDMSG = "This message must be used in a channel."
 
 
 def setup(bot):
+    # Configure the RC feed listener, save to bot memory, and start it.
     stop_event = threading.Event()
     url = "https://stream.wikimedia.org/v2/stream/recentchange"
     listen = threading.Thread(target=listener, args=(bot, url, stop_event))
     bot.memory["wikistream_stop"] = stop_event
     bot.memory["wikistream_listener"] = listen
+    bot.memory["wikistream_listener"].start()
 
+    # Configure the ORES feed listener, save to bot memory, and start it.
     ores_url = "https://stream.wikimedia.org/v2/stream/revision-score"
     ores = threading.Thread(target=ores_listener, args=(bot, ores_url, stop_event))
     bot.memory["ores_stop"] = stop_event
     bot.memory["ores"] = ores
+    bot.memory["ores"].start()
 
 
 def listener(bot, url, stop_event):
+    # Listen to EventStream for Recent Changes
     while not stop_event.is_set():
         for event in EventSource(url):
+            # Check for stop flag inside the loop
             if stop_event.is_set():
                 return
-            else:
-                if event.event == "message":
-                    try:
-                        change = json.loads(event.data)
-                        dispatch(bot, change)
-                    except ValueError:
-                        pass
+
+            if event.event == "message":
+                # Sometimes the EventStream sends garbage, discard if necessary
+                try:
+                    change = json.loads(event.data)
+                    dispatch(bot, change)
+                except ValueError:
+                    pass
 
 
 def ores_listener(bot, url, stop_event):
+    # Listen to EventStream for ORES events
     while not stop_event.is_set():
         for event in EventSource(url):
+            # Check for stop flag inside the loop
             if stop_event.is_set():
                 return
-            else:
-                if event.event == "message":
-                    try:
-                        change = json.loads(event.data)
-                        ores_dispatch(bot, change)
-                    except ValueError:
-                        pass
+
+            if event.event == "message":
+                # Sometimes the EventStream sends garbage, discard if necessary
+                try:
+                    change = json.loads(event.data)
+                    ores_dispatch(bot, change)
+                except ValueError:
+                    pass
 
 
 def dispatch(bot, change):
-
+    # Dispatch edits and page creations
     if change["type"] == "edit" or change["type"] == "new":
+        # Check for specific pages in watch
         if pagewatch.check(change):
             pagewatch.report(bot, change)
 
+        # Check for pages watched globally
         if globalwatch.check(change):
             globalwatch.report(bot, change)
 
-        if pagewatch.checkcss(change):
+        # If bot is Bot873, check for cssjs reporting
+        if (
+            pagewatch.checkcss(change)
+            and bot.nick == "Bot873"
+        ):
             pagewatch.cssjs(bot, change)
 
+    # If rc feed is being reported for a project, dispatch report
     if rcfeed.check(change):
         rcfeed.report(bot, change)
 
+    # If edits from un-confirmed accounts are being reported, dispatch report
     if confirmedfeed.check(change):
         confirmedfeed.report(bot, change)
 
+    # Dispatch Log events
     if change["type"] == "log":
+        # Handles GS Log events for GlobalSysBot
         if (
             gstools.check(change["wiki"])
             and bot.nick == "GlobalSysBot"
         ):
             gstools.report(bot, change)
+
+        # If abuse filter hits are being reported, dispatch report
         if change["log_type"] == "abusefilter":
             if affeed.check(change):
                 affeed.report(bot, change)
 
 
 def ores_dispatch(bot, change):
+    # Dispatch ORES reports
     if oresfeed.check(change):
         oresfeed.report(bot, change)
 
@@ -98,24 +121,24 @@ def ores_dispatch(bot, change):
 @plugin.require_admin(message=BOTADMINMSG)
 @plugin.require_chanmsg(message=CHANCMDMSG)
 @plugin.command("feedadmin")
-def feedcmd(bot, trigger):
+def feedcmd(bot, trigger): # !feedadmin {add/del/list} <target>
     cabalutil.feedadmin(bot, trigger)
 
 
 @plugin.command("speak")
-def set_speak(bot, trigger):
+def set_speak(bot, trigger): # Adds the channel to hushchannels where the bot will not speak
     cabalutil.watcherSpeak(bot, trigger)
 
 
 @plugin.command("hush")
 @plugin.command("mute")
-def do_hush(bot, trigger):
+def do_hush(bot, trigger): # Removes the channel from hushchannels and allows the bot to speak
     cabalutil.watcherHush(bot, trigger)
 
 
 @plugin.require_admin(message=BOTADMINMSG)
 @plugin.command("watchstart")
-def start_listener(bot, trigger):
+def start_listener(bot, trigger): # Manually restart listeners
     if "wikistream_listener" not in bot.memory:
         stop_event = threading.Event()
         bot.memory["wikistream_stop"] = stop_event
@@ -140,7 +163,7 @@ def start_listener(bot, trigger):
 
 
 @plugin.interval(120)
-def checkListener(bot):
+def checkListener(bot): # Verify listeners are still listening every 2 mins and restart if needed
     if bot.memory["wikistream_listener"].is_alive() is not True:
         del bot.memory["wikistream_listener"]
         del bot.memory["wikistream_stop"]
@@ -174,7 +197,7 @@ def checkListener(bot):
 
 @plugin.require_admin(message=BOTADMINMSG)
 @plugin.command("watchstatus")
-def watchStatus(bot, trigger):
+def watchStatus(bot, trigger): # Have the bot report the status of the listeners
     if (
         "wikistream_listener" in bot.memory
         and bot.memory["wikistream_listener"].is_alive() is True
@@ -194,7 +217,7 @@ def watchStatus(bot, trigger):
 
 @plugin.require_admin(message=BOTADMINMSG)
 @plugin.command("watchstop")
-def watchStop(bot, trigger):
+def watchStop(bot, trigger): # Force the listeners to halt and deletes the thread from memory
     if "wikistream_listener" not in bot.memory:
         bot.say("RC stream listener isn't running.")
     else:
@@ -218,7 +241,7 @@ def watchStop(bot, trigger):
 
 @plugin.require_chanmsg(message=CHANCMDMSG)
 @plugin.command("watch")
-def watch(bot, trigger):
+def watch(bot, trigger): # add a new specific page to watch on a specific project
     watchAction = trigger.group(3)
     if watchAction == "add" or watchAction == "Add" or watchAction == "+":
         if trigger.group(5) == "":
@@ -249,7 +272,7 @@ def watch(bot, trigger):
 # !globalwatch add namespaceid title
 @plugin.require_chanmsg(message=CHANCMDMSG)
 @plugin.command("globalwatch")
-def gwatch(bot, trigger):
+def gwatch(bot, trigger): # Globally watch a page in a certain namespace
     watchAction = trigger.group(3)
     if watchAction == "add" or watchAction == "Add" or watchAction == "+":
         if trigger.group(5) == "" or trigger.group(5) is None:
@@ -282,13 +305,13 @@ def gwatch(bot, trigger):
 
 @plugin.require_chanmsg(message=CHANCMDMSG)
 @plugin.command("namespace")
-def get_namespace(bot, trigger):
+def get_namespace(bot, trigger): # Get namespace information by either number or name
     bot.say(cabalutil.namespaces(trigger))
 
 
 @plugin.require_chanmsg(message=CHANCMDMSG)
 @plugin.command("abusefeed", "affeed")
-def do_affeed(bot, trigger):
+def do_affeed(bot, trigger): # !abusefeed {start/stop} <project> || Controls the Abuse Filter feed for this channel
     try:
         action, project = trigger.group(2).split(' ', 1)
     except ValueError:
@@ -305,7 +328,7 @@ def do_affeed(bot, trigger):
 
 @plugin.require_chanmsg(message=CHANCMDMSG)
 @plugin.command("rcfeed")
-def do_rcfeed(bot, trigger):
+def do_rcfeed(bot, trigger): # !rcfeed {start/stop} <project> || Controls the rc feed for this channel
     try:
         action, project = trigger.group(2).split(' ', 1)
     except ValueError:
@@ -322,6 +345,7 @@ def do_rcfeed(bot, trigger):
 @plugin.require_chanmsg(message=CHANCMDMSG)
 @plugin.command("confirmedfeed")
 def do_confirmedfeed(bot, trigger):
+    # !confirmedfeed {start/stop} <project> || Controls the Confirmed feed for this channel
     try:
         action, project = trigger.group(2).split(' ', 1)
     except ValueError:
@@ -338,7 +362,7 @@ def do_confirmedfeed(bot, trigger):
 
 @plugin.require_chanmsg(message=CHANCMDMSG)
 @plugin.command("oresfeed", "vandalfeed", "vandalismfeed")
-def do_oresfeed(bot, trigger):
+def do_oresfeed(bot, trigger): # # !oresfeed {start/stop} <project> || Controls the ORES feed for this channel
     try:
         action, project = trigger.group(2).split(' ', 1)
     except ValueError:
@@ -354,7 +378,7 @@ def do_oresfeed(bot, trigger):
 
 
 @plugin.find(r"\[\[(.*?)\]\]")
-def autolinker(bot, trigger):
+def autolinker(bot, trigger): # Autolinker for standard [[wikilinks]]
     if not autolink.checklang(trigger.sender) or cabalutil.ignored_nick(
         trigger.account
     ):
@@ -370,7 +394,7 @@ def autolinker(bot, trigger):
 
 
 @plugin.find(r"\{\{(.*?)\}\}")
-def autolinker_templates(bot, trigger):
+def autolinker_templates(bot, trigger): # Autolinker for {{Template:}} links
     if not autolink.checklang(trigger.sender) or cabalutil.ignored_nick(
         trigger.account
     ):
@@ -388,7 +412,7 @@ def autolinker_templates(bot, trigger):
 @plugin.require_admin(message=BOTADMINMSG)
 @plugin.require_chanmsg(message=CHANCMDMSG)
 @plugin.command("setlang")
-def setlang(bot, trigger):
+def setlang(bot, trigger): # Set the preferred language for a channel for Autolinks
     # !setlang enwiki https://enwp.org/
     if not trigger.group(4):
         bot.say("Missing URL! Syntax is !setlang <project> <baseURL>")
@@ -401,7 +425,7 @@ def setlang(bot, trigger):
 @plugin.require_admin(message=BOTADMINMSG)
 @plugin.require_chanmsg(message=CHANCMDMSG)
 @plugin.command("unsetlang")
-def unsetlang(bot, trigger):
+def unsetlang(bot, trigger): # Clear the set language for auto links (disables autolink)
     if autolink.rmvlang(trigger.sender):
         bot.say(trigger.sender + " was cleared.")
 
@@ -409,7 +433,7 @@ def unsetlang(bot, trigger):
 @plugin.require_admin(message=BOTADMINMSG)
 @plugin.require_chanmsg(message=CHANCMDMSG)
 @plugin.command("ignorenick")
-def setignorenick(bot, trigger):
+def setignorenick(bot, trigger): # Tell the bot to NOT do autolinks for the provided IRC account name
     # !ignorenick <ircAccountName>
     if autolink.ignorenick(trigger.group(3), trigger.account):
         bot.say("I'll ignore links from " + trigger.group(3) + " from now on.")
@@ -418,7 +442,7 @@ def setignorenick(bot, trigger):
 @plugin.require_admin(message=BOTADMINMSG)
 @plugin.require_chanmsg(message=CHANCMDMSG)
 @plugin.command("unignorenick")
-def setunignorenick(bot, trigger):
+def setunignorenick(bot, trigger): # Tell the bot to allow the IRC account to be autolinked again
     # !unignorenick <ircAccountName>
     if autolink.unignorenick(trigger.group(3)):
         bot.say("I'll stop ignoring links from " + trigger.group(3) + ".")
@@ -426,7 +450,7 @@ def setunignorenick(bot, trigger):
 
 @plugin.require_owner(message="This command is only available to the bot owner.")
 @plugin.command("restartbot", "restart")
-def do_bot_restart(bot, trigger):
+def do_bot_restart(bot, trigger): # Order the bot to restart with any provided message
     if trigger.group(2):
         bot.restart("Restarting by order of " + trigger.account + " Reason: " + trigger.group(2))
     else:
@@ -435,15 +459,19 @@ def do_bot_restart(bot, trigger):
 
 @plugin.require_owner(message="This command is only available to the bot owner.")
 @plugin.command("botdie", "quit")
-def do_botdie(bot, trigger):
+def do_botdie(bot, trigger): # Order the bot to quit with the provided message
     if trigger.group(2):
         bot.quit("Bot ordered to die by " + trigger.account + " Reason: " + trigger.group(2))
     else:
         bot.quit("Bot ordered to die by " + trigger.account)
 
 
-@plugin.command("onirc"):
-def do_onirc(bot, trigger):
+##########################################
+#      GlobalSysBot Commands follow      #
+##########################################
+
+@plugin.command("onirc")
+def do_onirc(bot, trigger): # Let GlobalSysBot
     if bot.nick != "GlobalSysBot":
         return
 
@@ -461,9 +489,7 @@ def do_onirc(bot, trigger):
     else:
         bot.say(data["msg"])
 
-##########################################
-#      GlobalSysBot Commands follow      #
-##########################################
+
 @plugin.require_admin(message=BOTADMINMSG)
 @plugin.command("addmember")
 def do_addmember(bot, trigger):
