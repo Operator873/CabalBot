875229101ec79fe43e360b32a121a21c1d67a26f
operator873 <operator873@gmail.com>
2022-09-07 17:59:42 -0500

    Initial commit to gitlabs

--- cabalbot/pushover.py
+++ cabalbot/pushover.py
@@ -2,9 +2,9 @@ import requests
 import cabalutil
 
 
-def send_alert(msg, group, bot):
+def send_alert(msg, bot):
     get_api_token = "SELECT data FROM config WHERE key='{}_pushover';".format(bot.lower())
-    get_group_token = "SELECT data FROM config WHERE key='{}';".format(group.lower())
+    get_group_token = "SELECT data FROM config WHERE key='{}_pushover_group';".format(bot.lower())
     pushover = "https://api.pushover.net/1/messages.json"
 
     api_token = cabalutil.do_sqlite(get_api_token, "one")[0]
@@ -13,7 +13,7 @@ def send_alert(msg, group, bot):
     alert = {
         "token": api_token,
         "user": group_token,
-        "title": "CabalBot Alert!",
+        "title": "{} Alert!".format(bot),
         "message": msg['data'],
         "priority": msg['priority']
     }
